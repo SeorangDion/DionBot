d9b236da6125976d99f2512f4306f62a7ab7cae5
Dion <92658890+SeorangDion@users.noreply.github.com>
2022-05-13 16:40:45 +0700

    Update start.py

--- DionRobot/modules/start.py
+++ DionRobot/modules/start.py
@@ -10,24 +10,26 @@ START_TEXT = """
 **Click the below button for getting help menu!**
 """
 
+
+START_BTN = [
+                [
+                    Button.url("Add me to your group", "https://t.me/DionRobot?startgroup")
+                ],
+                [
+                    Button.url("Repo", "https://github.com/SeorangDion/DionBot"),
+                    Button.url("Updates", "https://t.me/DionProjects")
+                ],
+                [
+                    Button.inline("Help And Commands", data="help")
+                ]
+            ]
+
+
 @dion.on(events.NewMessage(pattern="^[?!/]start$"))
 async def start(event):
 
     if event.is_private:
-       await event.reply(START_TEXT.format(event.sender.first_name), 
-        buttons=
-        [
-            [
-                Button.url("Add me to your group", "https://t.me/DionRobot?startgroup")
-            ],
-            [
-                Button.url("Repo", "https://github.com/SeorangDion/ManagerBot"),
-                Button.url("Updates", "https://t.me/DionProjects")
-            ],
-            [
-                Button.inline("Help And Commands", data="help")
-            ]
-        ]
+       await event.reply(START_TEXT.format(event.sender.first_name), buttons=START_BTN
        )
        return
 
@@ -42,3 +44,8 @@ async def start(event):
         ]
        )
        return
+
+
+@dion.on(events.callbackquery.CallbackQuery(data="home"))
+async def hstart(event):
+     await event.edit(START_TEXT, buttons=START_BTN)
